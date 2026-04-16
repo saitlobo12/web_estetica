3485b03725fe69eb8eb91cc09b061da2eeec188d
saitlobo12 <saitlobo13@gmail.com>
2026-04-16 15:00:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-// Botón WhatsApp
+// Botón WhatsApp 
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", () => {
     window.open("https://wa.me/573225709880", "_blank");
 });
+
+
+// 🔥 SLIDER ANTES / DESPUÉS
+const slider = document.querySelector('.slider');
+const before = document.querySelector('.img-before');
+
+if (slider && before) {
+    slider.addEventListener('input', function() {
+        before.style.width = this.value + '%';
+    });
+}
